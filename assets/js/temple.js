c5a43268853bd76bf803c05a7937ec78222b598b
tu-chill <c.hill@temple.edu>
2018-10-01 12:46:19 -0400

    clean out old

--- assets/js/temple.js
+++ assets/js/temple.js
@@ -9,6 +9,7 @@ $( document ).ready(function() {
 // });
 // initialization of collapsible elements
  $('.collapsible').collapsible();
+ $(".sidenav").sidenav();
 
 // Nav Toggle
 $('[nav-toggle]').click(function(){
