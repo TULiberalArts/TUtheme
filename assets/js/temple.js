1bb57f64b5142031b625962052fc5182248dd08f
tu-chill <c.hill@temple.edu>
2018-10-08 11:52:36 -0400

    move into init

--- assets/js/temple.js
+++ assets/js/temple.js
@@ -1,15 +1,44 @@
 $( document ).ready(function() {
-
-// Smooth Scroll
-// $('a:not(.no-scroll)').click(function(){
-//     $('html, body').animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top
-//     }, 500);
-//     return false;
-// });
-// initialization of collapsible elements
+// initialization of stuff
  //$('.collapsible').collapsible();
  //$(".sidenav").sidenav();
+ //Smoth scroll
+ // Select all links with hashes
+ $('a[href*="#"]')
+   // Remove links that don't actually link to anything
+   .not('[href="#"]')
+   .not('[href="#0"]')
+   .click(function(event) {
+     // On-page links
+     if (
+       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+       &&
+       location.hostname == this.hostname
+     ) {
+       // Figure out element to scroll to
+       var target = $(this.hash);
+       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+       // Does a scroll target exist?
+       if (target.length) {
+         // Only prevent default if animation is actually gonna happen
+         event.preventDefault();
+         $('html, body').animate({
+           scrollTop: target.offset().top
+         }, 1000, function() {
+           // Callback after animation
+           // Must change focus!
+           var $target = $(target);
+           $target.focus();
+           if ($target.is(":focus")) { // Checking if the target was focused
+             return false;
+           } else {
+             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+             $target.focus(); // Set focus again
+           };
+         });
+       }
+     }
+   });
 
 // Nav Toggle
 $('[nav-toggle]').click(function(){
@@ -34,43 +63,6 @@ $(document).keydown(function(e){
 });
 
 });
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
 
 // sttream lining our marketing pay per view form validation without the bloat of mailchimp js
 //https://css-tricks.com/form-validation-part-4-validating-mailchimp-subscribe-form/
