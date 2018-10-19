1ca5fb79724af5c67e6b997e9cd8e6e9862dfbbd
Sam Pizzo <tuk52222@temple.edu>
2018-10-19 14:13:46 -0400

    sidenav

--- assets/js/temple.js
+++ assets/js/temple.js
@@ -60,7 +60,15 @@ $(document).ready(function() {
   //     }
   //   }
   // });
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      //edge: 'right', // Choose the horizontal origin
+      //closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      //onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      //onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    }
+  );
 });
 
 // sttream lining our marketing pay per view form validation without the bloat of mailchimp js
